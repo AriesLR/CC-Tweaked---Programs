62aff2ef9d86100c4c781d9533c8fffd7262ffcf
Bobjobob24 <bobsgaming24@gmail.com>
2026-09-06 16:04:10 -0500

    .

---
+++ programs/elevator-floor-display/install_send.lua
@@ -0,0 +1,43 @@
+local scriptUrl = "https://raw.githubusercontent.com/AriesLR/CC-Tweaked---Programs/refs/heads/main/programs/elevator-floor-display/src/send_floor.lua"
+local updateUrl = "https://raw.githubusercontent.com/AriesLR/CC-Tweaked---Programs/refs/heads/main/programs/elevator-floor-display/src/update_send.lua"
+local uninstallUrl = "https://raw.githubusercontent.com/AriesLR/CC-Tweaked---Programs/refs/heads/main/programs/elevator-floor-display/src/uninstall.lua"
+local targetDir = "/alr/elevator-floor-display"
+
+-- Ensure the directory exists
+if not fs.exists(targetDir) then
+    print("Creating directory " .. targetDir)
+    fs.makeDir(targetDir)
+end
+
+-- Download from raw URL
+local function downloadFile(url, dir)
+    local fileName = url:match(".+/([^/]+)$")
+    local fullPath = dir .. "/" .. fileName
+
+    print("Downloading " .. fileName .. " to " .. dir .. "...")
+    local res = http.get(url)
+    if res then
+        local f = fs.open(fullPath, "w")
+        f.write(res.readAll())
+        f.close()
+        res.close()
+        print(fileName .. " downloaded successfully.")
+    else
+        print("Failed to download " .. fileName)
+    end
+
+    return fullPath
+end
+
+-- Download main script
+local mainScriptPath = downloadFile(scriptUrl, targetDir)
+
+-- Download uninstaller
+downloadFile(uninstallUrl, targetDir)
+
+-- Download updater
+downloadFile(updateUrl, targetDir)
+
+-- Run the main script
+print("Running " .. mainScriptPath .. "...")
+shell.run(mainScriptPath)
