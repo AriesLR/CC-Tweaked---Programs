649b99dda1246385f6e2078c75e3493733c833ca
Bobjobob24 <bobsgaming24@gmail.com>
2026-09-06 16:39:15 -0500

    fix

--- programs/elevator-floor-display/src/display_floor.lua
+++ programs/elevator-floor-display/src/display_floor.lua
@@ -1,5 +1,4 @@
 local CHANNEL = 9000
-
 local monitor = peripheral.find("monitor")
 local modem = peripheral.find("modem")
 
@@ -9,25 +8,31 @@ if not modem then error("No Ender Modem attached!") end
 modem.open(CHANNEL)
 monitor.setTextScale(3)
 
-local function drawDisplay(floor)
+local function drawDisplay(floor, isMoving)
     monitor.setBackgroundColor(colors.black)
     monitor.clear()
     
+    -- Header
     monitor.setCursorPos(1, 1)
     monitor.setTextColor(colors.gray)
     monitor.write("FLOOR")
     
+    -- Floor Name / Number
     monitor.setCursorPos(1, 2)
-    monitor.setTextColor(colors.lime)
+    if isMoving then
+        monitor.setTextColor(colors.yellow)
+    else
+        monitor.setTextColor(colors.lime)
+    end
     monitor.write(tostring(floor))
 end
 
-drawDisplay("--")
+drawDisplay("--", false)
 
 while true do
     local _, _, sendChannel, _, message = os.pullEvent("modem_message")
     
     if sendChannel == CHANNEL and type(message) == "table" and message.floor then
-        drawDisplay(message.floor)
+        drawDisplay(message.floor, message.moving)
     end
 end
--- programs/elevator-floor-display/src/send_floor.lua
+++ programs/elevator-floor-display/src/send_floor.lua
@@ -1,30 +1,24 @@
 local CHANNEL = 9000
-
--- Local Source Block directly attached to this computer
-local source = peripheral.find("create_source") or peripheral.find("source")
+local lift = peripheral.find("create_elevator")
 local modem = peripheral.find("modem")
 
-if not source then error("Source Block not found next to this computer!") end
-if not modem then error("Ender/Wireless Modem not found!") end
+if not lift then error("Create Elevator peripheral not found!") end
+if not modem then error("Ender Modem not found!") end
 
 local lastFloor = nil
 
 while true do
-    local currentFloor = nil
+    -- getNearestFloor() returns the nearest floor name/identifier
+    local floorName, floorY = lift.getNearestFloor()
     
-    -- Query floor from CC:C Bridge Source Block
-    if source.getCurrentFloor then
-        currentFloor = source.getCurrentFloor()
-    elseif source.getDetail then
-        local detail = source.getDetail()
-        currentFloor = detail and detail.floor
+    if floorName and floorName ~= lastFloor then
+        modem.transmit(CHANNEL, CHANNEL, { 
+            floor = floorName,
+            moving = lift.isMoving(),
+            state = lift.getState()
+        })
+        lastFloor = floorName
     end
-
-    -- Broadcast wirelessly when floor changes
-    if currentFloor and currentFloor ~= lastFloor then
-        modem.transmit(CHANNEL, CHANNEL, { floor = currentFloor })
-        lastFloor = currentFloor
-    end
-
-    sleep(0.2)
+    
+    sleep(0.1)
 end
